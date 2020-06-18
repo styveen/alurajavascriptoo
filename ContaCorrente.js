759cf5df0b8132f29d9b481ac88bb53480351427
David Styveen <dstyveen@gmail.com>
2020-06-18 14:41:58 -0300

    Finalização do curso

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,10 +1,34 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente {
-    cliente;
+    static numeroDeContas = 0;
     agencia;
     numConta;
+    _cliente;
+    _saldo = 0; // convenção -- #saldo (definir que a propriedade é privada)
 
-    // #saldo (definir que a propriedade é privada)
-    _saldo = 0; // convenção 
+    constructor(agencia, numConta, cliente) {
+        this.agencia = agencia;
+        this.numConta = numConta;
+        this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
+    }
+
+    set cliente(novoValor) {
+
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
+        }
+
+    }
+
+    get cliente() {
+        return this._cliente;
+    }
+
+    get saldo() {
+        return this._saldo;
+    }
 
     depositar(valor) {
         if (valor > 0) {
--- index.js
+++ index.js
@@ -2,31 +2,17 @@ import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
 // definicao cliente 1
-const cliente = new Cliente();
-const contaCliente = new ContaCorrente();
-cliente.nome = "Styveen";
-cliente.cpf = 11122233309;
-
-contaCliente.cliente = cliente;
-contaCliente.agencia = 1001;
-contaCliente.numConta = 1;
+const cliente = new Cliente("Styveen", 11122233309);
+const contaCliente = new ContaCorrente(1001, 1, cliente);
 
 // definicao cliente 2
+const cliente2 = new Cliente("Raul", 54321194510);
+const contaCliente2 = new ContaCorrente(1001, 2, cliente2);
 
-const cliente2 = new Cliente();
-const contaCliente2 = new ContaCorrente();
-
-cliente2.nome = "Raul";
-cliente2.cpf = 54321194510;
-
-contaCliente2.cliente = cliente2;
-contaCliente2.agencia = 1001;
-contaCliente2.numConta = 2;
-
-// deposito conta cliente 1 
 console.log("\n\n*** Início ***");
 console.log(`Saldo inicial: ${contaCliente._saldo}`);
 
+// deposito conta cliente 1 
 const valorDeposito = 200;
 contaCliente.depositar(valorDeposito);
 
@@ -47,6 +33,7 @@ console.log("\n\n*** Transferência ***");
 console.log(cliente, `\nValor solicitado: ${valorTransferir} \nValor transferido: ${valorTransferido} \nSaldo disponível: ${contaCliente._saldo} \n`);
 console.log(cliente2, `\nValor provisionado: ${valorTransferir} \nValor recebido: ${valorTransferido} \nSaldo disponível: ${contaCliente2._saldo}`);
 
+console.log(ContaCorrente.numeroDeContas);
 /*
 terminal: npm init
 */
